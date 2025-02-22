2f3fe9b8a1e375761273d47bac90a2f5de8f30b3
Ashritha <preethiashrithagunnam@gmail.com>
2025-02-22 11:24:59 +0530

    Update: Modified code again

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -42,7 +42,7 @@ const Home = () => {
 
                 <div className="image-container">
                     <img 
-                        src="public\assets\Thub.jpg" 
+                        src="../public/assets/Thub.jpg" 
                         alt="Preethi" 
                         className="profile-image"
                     />
